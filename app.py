040984160e78bd734a1d828d1d8b70dcc312911f
Kate <chenk18@outlook.com>
2024-04-24 00:01:11 -0700

    final version

--- app.py
+++ app.py
@@ -3,34 +3,48 @@ import os
 import google.generativeai as genai
 from dotenv import load_dotenv
 import streamlit as st
+import time 
 
 load_dotenv()
 
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 model = genai.GenerativeModel('gemini-pro')
 
 prompt_template = """
-You are an expert at planning oversea trips.
+You are an expert at answering job application questions.
 
-Please take the users request and plan a comprehensive trip for them.
+Please use the information mentioned in resume and job description to answer job application questions.
 
 Please include the following details:
-- The destination
-- The duration of the trip
-- The activities that will be done
-- The accommodation
+- Bullet points
+- put a key word for each point on the beginning
 
-The user's request is:
-{prompt}
+my resume is: {resume}
+the jd: {jd}
+the question: {question}
 """
 
-def generate_content(prompt):
+def generate_content(resume, jd, question):
+    prompt = prompt_template.format(resume = resume, jd = jd, question = question)
     response = model.generate_content(prompt)
     return response.text
 
-st.title("🏝️ AI Travel Planning")
+def stream_output():
+    for word in reply.split(" "):
+        yield word + " "
+        time.sleep(0.02)
 
-prompt = st.text_area("Enter your next travel request (days, destination, activities, etc.):")
-if st.button("Give me a plan!"):
-    reply = generate_content(prompt)
-    st.write(reply)
+st.title("AI Job Application Questions Bot")
+
+c1, c2 = st.columns(2)
+
+with c1:
+    resume = st.text_area("Enter your CV:", height = 300)
+with c2:
+    jd = st.text_area("Enter the job description:", height = 300)
+
+question = st.text_input("Enter the application question: ")
+reply = None
+if st.button("Give me an answer!", use_container_width=True):
+    reply = generate_content(resume, jd, question)
+    st.write_stream(stream_output)
